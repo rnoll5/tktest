a8f7e2e0a905c4e2973eaf2fe9667dccb11bf6ba
RNoll <rnoll@softstackfactory.org>
2016-11-04 17:40:27 +0000

    removed lobby back button

--- www/js/controllers/LobbyCtrl.js
+++ www/js/controllers/LobbyCtrl.js
@@ -1,7 +1,7 @@
 angular.module('starter.controllers')
-.controller('LobbyCtrl',['$scope', 'TKTestQuestionService', '$state', 'TKAnswersService', '$window', 'SSFUsersRest',
+.controller('LobbyCtrl',['$scope', 'TKTestQuestionService', '$state', 'TKAnswersService', '$window', 'SSFUsersRest', '$ionicHistory',
 
-function($scope, TKTestQuestionService, $state, TKAnswersService, $window, SSFUsersRest) {
+function($scope, TKTestQuestionService, $state, TKAnswersService, $window, SSFUsersRest, $ionicHistory) {
 TKTestQuestionService.all();
 
  $scope.goToTest = function()
@@ -16,13 +16,10 @@ TKTestQuestionService.all();
   };
    
  $scope.logout = function() {
-  SSFUsersRest.logout($window.localStorage.token)
-   .then(function(response) {
-    if (response.status == 204) {
-     $state.go('landing');
-    } else {
-     $state.go('landing');
-    }
-   });
- }
+  SSFUsersRest.logout($window.localStorage.token);
+   $ionicHistory.nextViewOptions({
+   historyRoot: true
+  });
+   $state.go('landing');
+   };
  }]);
